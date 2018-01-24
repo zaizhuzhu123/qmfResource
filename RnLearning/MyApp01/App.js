afafc598b8b22025950d8e1b6a82ffb13d620739
qmf <651857512@qq.com>
2018-01-24 10:53:18 +0800

    20180124
    20180124

---
+++ RnLearning/MyApp01/App.js
@@ -0,0 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import Button from "react-native-button";
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <Text style={{
+                    position: 'absolute',
+                    width: '100%',
+                    top: 0,
+                    height: 50,
+                    fontSize: 20,
+                    backgroundColor: '#00A0E9',
+                    textAlign: 'center',
+                    lineHeight: 50
+                }}>
+                    1111
+                </Text>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!dfgdfgdf2
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit App.js
+                </Text>
+                <Text style={styles.instructions}>
+                    {instructions}
+                </Text>
+                <View style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    width: '100%',
+                    flex: 1,
+                    justifyContent: 'space-around',
+                    backgroundColor: '#00A0E9',
+                    alignItems: 'center',
+                    flexDirection: 'row'
+                }}>
+                    <Button style={{minWidth: '20%', textAlign: 'center', color: '#fff'}}>一</Button>
+                    <Button style={{minWidth: '20%', textAlign: 'center', color: '#fff'}}>二</Button>
+                    <Button style={{minWidth: '20%', textAlign: 'center', color: '#fff'}}>三</Button>
+                    <Button style={{minWidth: '20%', textAlign: 'center', color: '#fff'}}>四</Button>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#000',
+        marginBottom: 5,
+    },
+});
